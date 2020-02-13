7883885a1443e5186366af472e9ceb7f73bdf5f7
Léopold Mols <53212@etu.he2b.be>
2020-02-13 17:17:35 +0100

    Git working properly

---
+++ ANLL/TD01/atlg4-intro/src/test/java/AppTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author leopoldmols
+ */
+public class AppTest {
+    
+    public AppTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+
+    /**
+     * Test of sum method, of class App.
+     */
+    @Test
+    public void testSum() {
+        System.out.println("testSum general case"); //Arrange
+        int nb1 = 10;
+        int nb2 = 32;
+        //Action
+        App app = new App();
+        int result = app.sum(nb1, nb2);
+        //Assert
+        int expected = 42;
+        assertEquals(expected, result);
+    }
+    
+}
